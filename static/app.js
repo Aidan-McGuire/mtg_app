fadc02bdee0e143a9447a4e801ec7fdf852014dd
Aidan McGuire <aidanmcguire211@gmail.com>
2026-08-22 09:23:02 -0600

    feat(collection): dispatch Group: Type to groupCardsByType in renderCollectionGrid

--- static/app.js
+++ static/app.js
@@ -1608,7 +1608,11 @@ function renderCollectionGrid() {
     return;
   }
 
-  if (collectionState.groupBy !== 'none') {
+  if (collectionState.groupBy === 'type') {
+    const groups = groupCardsByType(filtered);
+    for (const g of groups) g.cards.sort(cmp);
+    renderGroupedGrid(grid, groups, card => buildCardTile(card, { showOwnedBadge: false }), { collapsedState: collectionGroupCollapsed });
+  } else if (collectionState.groupBy === 'collection-tag') {
     const groups = groupCards(filtered, 'collection_tags');
     for (const g of groups) g.cards.sort(cmp);
     renderGroupedGrid(grid, groups, card => buildCardTile(card, { showOwnedBadge: false }), { collapsedState: collectionGroupCollapsed });
